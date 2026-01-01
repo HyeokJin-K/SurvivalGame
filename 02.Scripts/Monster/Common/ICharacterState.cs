ac6d13ea1baef09ae6dce714fae962df23aaee50
HyeokJin-K <39337614+HyeokJin-K@users.noreply.github.com>
2026-01-02 06:00:19 +0900

    .

--- 02.Scripts/Monster/Common/ICharacterState.cs
+++
@@ -1,6 +0,0 @@
-﻿public interface ICharacterState
-{
-    void Enter();
-    void Update();
-    void Exit();
-}
--- 02.Scripts/Projectile/Interface/IProjectile.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IProjectile
-{
-    void Fire(ProjectileContext ctx); // 생성 직후, 소유자가 호출
-}
